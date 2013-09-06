a086f5f7fea4051eef247da21d82f0faf7db19df
Duane Jeffers <duane@duanejeffers.com>
2013-09-06 01:01:33 -0700

    Removing logging for master repo for pushing to Github

--- bloog.php
+++ bloog.php
@@ -14,8 +14,6 @@ define('BLOOG_CFG', '.bloogconfig.php');
 define('PATH', '/');
 define('CONT_EXT', '.md');
 define('ANCHOR', '<a href="%s" class="%s">%s</a>');
-define('DEV_LOG', TRUE);
-define('DEV_LOG_LOC', '/var/log/bloog.log');
 
 require_once('vendor/autoload.php');
 
