a53a47acc78ed6fa1bb2f78d1dd7a390daf9ee40
Duane Jeffers <duane@duanejeffers.com>
2013-09-13 21:12:32 -0700

    Re-enabled logging on bloog

--- bloog.php
+++ bloog.php
@@ -13,7 +13,7 @@
 define('BLOOG_CFG', '.bloogconfig.php');
 define('PATH', '/');
 define('CONT_EXT', '.md');
-define('DEV_LOG', FALSE);
+define('DEV_LOG', TRUE);
 define('DEV_LOG_LOC', '/var/log/bloog.log');
 
 require_once('vendor/autoload.php');
